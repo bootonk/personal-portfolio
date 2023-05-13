13de2891d10efab312afbf0fde81d2af0c01e74f
Kate <115913025+bootonk@users.noreply.github.com>
2023-05-13 10:35:56 -0700

    Add Reading content

--- src/components/Reading.js
+++ src/components/Reading.js
@@ -10,19 +10,33 @@ export default function Reading() {
           <h3>I'm reading...</h3>
         </div>
         <div className="back">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            nec lobortis dolor. Fusce in sagittis arcu. Praesent quis nunc
-            bibendum enim feugiat hendrerit sagittis id augue. Donec commodo
-            egestas scelerisque. Ut placerat neque neque, vitae venenatis lectus
-            pharetra eu. Cras commodo velit sed egestas vulputate. Aliquam erat
-            volutpat. Sed pretium in tortor sed sodales. Nullam nec egestas
-            massa, quis volutpat risus. Nullam ex eros, dapibus id sapien eu,
-            tincidunt varius nulla. Aliquam mauris risus, hendrerit sit amet
-            mauris rhoncus, rhoncus pellentesque mauris. Cras lorem mauris,
-            euismod in leo a, iaculis viverra mauris. Donec velit nisl,
-            sollicitudin et malesuada in, dapibus ut nulla.
-          </p>
+          <ul>
+            <li>
+              <p>
+                <a href="https://www.goodreads.com/en/book/show/4099">
+                  The Pragmatic Programmer
+                </a>{" "}
+                by Andrew Hunt & David Thomas. (The 20th Anniversary Edition of
+                course!)
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="https://www.goodreads.com/book/show/35074096-mythos?ref=nav_sb_ss_1_6">
+                  Mythos: The Greek Myths Retold
+                </a>{" "}
+                by Stephen Fry
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="https://www.goodreads.com/book/show/24983.Doomsday_Book?ac=1&from_search=true&qid=9WK22R6M4N&rank=1">
+                  Dooms Day Book
+                </a>{" "}
+                by Connie Willis
+              </p>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
