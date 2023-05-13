367ac0df35f9d0a32213564509c24448558b8701
Kate <115913025+bootonk@users.noreply.github.com>
2023-05-12 21:20:09 -0700

    Layout structure, styling, and assets

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Bio from "./components/Bio";
 import Learning from "./components/Learning";
 import Doing from "./components/Doing";
 import Reading from "./components/Reading";
+import Contact from "./components/Contact";
+
+import duck from "./assets/rubber-duck.png";
 
 function App() {
   return (
     <div className="App">
+      <img src={duck} alt="duck" className="header-img" />
       <h1>Hi, I'm Kate!</h1>
-      <Bio />
-      <Reading />
-      <Learning />
-      <Doing />
+      <div className="block-container">
+        <Bio />
+        <Learning />
+        <Reading />
+        <Doing />
+      </div>
+      <Contact />
     </div>
   );
 }
