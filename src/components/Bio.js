fcedfede2d2687bc191721ba3637240190acd266
Kate <115913025+bootonk@users.noreply.github.com>
2023-05-14 18:53:14 -0700

    Add responsive styling for mobile and tablet

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,15 +4,15 @@ import mug from "../assets/mug.png";
 export default function Bio() {
   return (
     <div className="flip-block">
-      <h3>I'm...</h3>
+      <h3>I'm a...</h3>
       <div className="content">
         <div className="front">
           <img src={mug} alt="mug" className="flip-img" />
         </div>
         <div className="back">
           <p>
-            A creator through and through. I collect skills and hobbies and try
-            to find interesting ways to combine them.
+            Creator through and through. I collect skills and hobbies and try to
+            find interesting ways to combine them.
           </p>
           <p>
             I care about community and supporting diverse spaces. The more
