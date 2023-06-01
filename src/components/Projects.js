736e3182c96896b7cafa596d2e6a0da3b2632ea1
Kate <115913025+bootonk@users.noreply.github.com>
2023-06-01 15:33:20 -0700

    Add QR Cross stitch project and assets

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,12 +7,14 @@ import Baseball from "./project-components/Baseball";
 import Personal from "./project-components/Personal";
 import Projector from "./project-components/Projector";
 import Smart from "./project-components/Smart";
+import QR from "./project-components/QR";
 
 // Project Modes
 const TRYPODS = "try-pods";
 const PERSONAL = "personal-portfolio";
 const SMART = "smart-todo";
 const BASEBALL = "star-wars-major-league";
+const QRX = "qr-cross-stitch";
 const PROJECTOR = "projector-sewing";
 
 export default function Projects() {
@@ -34,6 +36,10 @@ export default function Projects() {
           Smart To-Do
         </a>{" "}
         /{" "}
+        <a href="#/" onClick={() => setProject("qr-cross-stitch")}>
+          QR Cross Stitch
+        </a>{" "}
+        /{" "}
         <a href="#/" onClick={() => setProject("star-wars-major-league")}>
           Star Wars Major League
         </a>{" "}
@@ -46,6 +52,7 @@ export default function Projects() {
         {project === TRYPODS && <TryPods />}
         {project === PERSONAL && <Personal />}
         {project === SMART && <Smart />}
+        {project === QRX && <QR />}
         {project === BASEBALL && <Baseball />}
         {project === PROJECTOR && <Projector />}
       </div>
